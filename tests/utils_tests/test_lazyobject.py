felixxm/django

commit 97d7990abde3fe4b525ae83958fd0b52d6a1d13f

Fixed #28358 -- Prevented LazyObject from mimicking nonexistent attributes.
Thanks Sergey Fedoseev for the initial patch.

django/utils/functional.py
@@ -266,6 +266,7 @@ def new_method_proxy(func):
             self._setup()
         return func(self._wrapped, *args)
 
+    inner._mask_wrapped = False
     return inner
 
 
@@ -285,6 +286,14 @@ class LazyObject:
         # Note: if a subclass overrides __init__(), it will likely need to
         # override __copy__() and __deepcopy__() as well.
         self._wrapped = empty
+
+    def __getattribute__(self, name):
+        value = super().__getattribute__(name)
+        # If attribute is a proxy method, raise an AttributeError to call
+        # __getattr__() and use the wrapped object method.
+        if not getattr(value, "_mask_wrapped", True):
+            raise AttributeError
+        return value
 
     __getattr__ = new_method_proxy(getattr)
 

tests/utils_tests/test_lazyobject.py
@@ -32,6 +32,28 @@ class LazyObjectTestCase(TestCase):
 
         return AdHocLazyObject()
 
+    def test_getattribute(self):
+        """
+        Proxy methods don't exist on wrapped objects unless they're set.
+        """
+        attrs = [
+            "__getitem__",
+            "__setitem__",
+            "__delitem__",
+            "__iter__",
+            "__len__",
+            "__contains__",
+        ]
+        foo = Foo()
+        obj = self.lazy_wrap(foo)
+        for attr in attrs:
+            with self.subTest(attr):
+                self.assertFalse(hasattr(obj, attr))
+                setattr(foo, attr, attr)
+                obj_with_attr = self.lazy_wrap(foo)
+                self.assertTrue(hasattr(obj_with_attr, attr))
+                self.assertEqual(getattr(obj_with_attr, attr), attr)
+
     def test_getattr(self):
         obj = self.lazy_wrap(Foo())
         self.assertEqual(obj.foo, "bar")

django/conf/__init__.py
@@ -157,8 +157,9 @@ class LazySettings(LazyObject):
     def USE_L10N(self):
         stack = traceback.extract_stack()
         # Show a warning if the setting is used outside of Django.
-        # Stack index: -1 this line, -2 the caller.
-        filename, _, _, _ = stack[-2]
+        # Stack index: -1 this line, -2 the LazyObject __getattribute__(),
+        # -3 the caller.
+        filename, _, _, _ = stack[-3]
         if not filename.startswith(os.path.dirname(django.__file__)):
             warnings.warn(
                 USE_L10N_DEPRECATED_MSG,

commit 1f663972bab1518eeebc3649ce45bd59df6c46e5

Refs #28358 -- Fixed an infinite recursioon bug introduced by 97d7990abde3fe4b525ae83958fd0b52d6a1d13f
Co-authored-by: Matthias Kestenholz <mk@feinheit.ch>

django/utils/functional.py
@@ -288,14 +288,19 @@ class LazyObject:
         self._wrapped = empty
 
     def __getattribute__(self, name):
-        value = super().__getattribute__(name)
-        # If attribute is a proxy method, raise an AttributeError to call
-        # __getattr__() and use the wrapped object method.
-        if not getattr(value, "_mask_wrapped", True):
-            raise AttributeError
+        __getattribute__ = super().__getattribute__
+        found = True
+        try:
+            value = __getattribute__(name)
+        except AttributeError:
+            found = False
+        # If attribute is a proxy method or it wasn't found, initialize the
+        # wrapped object and call its __getattr__().
+        if not found or not getattr(value, "_mask_wrapped", True):
+            if __getattribute__("_wrapped") is empty:
+                __getattribute__("_setup")()
+            return getattr(__getattribute__("_wrapped"), name)
         return value
-
-    __getattr__ = new_method_proxy(getattr)
 
     def __setattr__(self, name, value):
         if name == "_wrapped":

tests/utils_tests/test_lazyobject.py
@@ -65,6 +65,14 @@ class LazyObjectTestCase(TestCase):
         self.assertEqual(obj.foo, "BAR")
         self.assertEqual(obj.bar, "baz")
 
+    def test_lazy_with_getattr_override(self):
+        class Thing:
+            def __getattr__(self, key):
+                return []
+
+        obj = self.lazy_wrap(Thing())
+        self.assertEqual(obj.main, [])
+
     def test_setattr2(self):
         # Same as test_setattr but in reversed order
         obj = self.lazy_wrap(Foo())